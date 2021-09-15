8030758fc34f7f201879fa6c08a3de7c5c3133f4
Carlos Passos <carlospassos93@gmail.com>
2021-09-15 17:48:50 -0300

    feat: product content

--- src/pages/product/[Slug].tsx
+++ src/pages/product/[Slug].tsx
@@ -1,17 +1,61 @@
 import Image from 'next/image';
 import { BackButton } from '../../components/BackButton';
+import { formatPrice } from '../../utils/formatPrice';
+
+import ShoppingBag from '../../../public/shopping-bag.svg'
 
 import {
   Container,
   Content,
+  ProductContent,
+  ProductHeader,
+  Description,
+  AddToCartButton,
+
 } from './styles'
+
+const productImageLoader = ({ src, width, quality }) => {
+  return `/${src}?w=${width}&q=${quality || 75}`
+}
 
 export default function Product() {
   return (
     <Container>
       <BackButton />
       <Content>
-        {/* <Image */}
+        <Image
+              loader={productImageLoader}
+              src="products/canecas/caneca_01.jpg"
+              alt="Caneca de cerâmica rústica"
+              width={640}
+              height={580}
+              objectFit="cover"
+            />
+        <ProductContent>
+          <ProductHeader>
+            <span>Caneca</span>
+            <div>
+              <h2>Caneca de cerâmica rústica</h2>
+              <span>{formatPrice(40)}</span>
+              <p>*Frete de R$40,00 para todo o Brasil. Grátis para compras acima de R$900,00.</p>
+            </div>
+          </ProductHeader>
+          
+          <Description>
+            <h3>Descrição</h3>
+            <p>
+              Aqui vem um texto descritivo do produto, esta caixa de texto servirá
+              apenas de exemplo para que simule algum texto que venha a ser inserido
+              nesse campo, descrevendo tal produto.
+            </p>
+          </Description>
+          
+
+          <AddToCartButton>
+            <ShoppingBag />
+            Adicionar ao carrinho
+          </AddToCartButton>
+        </ProductContent>
       </Content>
     </Container>
   )
--- src/pages/product/styles.ts
+++ src/pages/product/styles.ts
@@ -1,10 +1,107 @@
 import styled from 'styled-components'
 
   export const Container = styled.div`
-
+    display: flex;
+    flex-direction: column;
   `;
   
   export const Content = styled.div`
+    display: grid;
+    grid-template-columns: 1.5fr 1fr;
+    gap: 32px;
+    margin-top: 22px;
+
+    &>div>img {
+      width: 100%;
+      border-radius: 4px;
+    }
+  `;
+
+
+  export const ProductContent = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    color: ${({theme}) => theme.colors.texts.dark};
+  `;
+
+  export const ProductHeader = styled.div`
+    
+    &>span {
+      font-size: 16px;
+      display: inline-block;
+      margin-bottom: 12px;
+    }
+
+    &>div {
+      margin-bottom: 58px;
+
+      h2 {
+        font-size: 32px;
+        font-weight: 300;
+      }
+
+      span {
+        display: inline-block;
+        
+        font-weight: 600;
+        font-size: 20px;
+        
+        margin-bottom: 24px;
+      }
+
+      &>p {
+        font-weight: normal;
+        font-size: 12px;
+      }
+    }
+
     
   `;
+
+  export const Description = styled.div`
+    &>h3 {
+      text-transform: uppercase;
+      font-weight: 500;
+      font-size: 16px;  
+      color: ${({theme}) => theme.colors.texts.light};
+
+      margin-bottom: 8px;
+
+    }
+
+    p {
+      font-weight: 400;
+      font-size: 14px;
+    }
+  `;
+
+  export const AddToCartButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-self: flex-end;
+    margin-top: auto;
+
+    height: 44px;
+    background-color: ${({theme}) => theme.colors.brand.blue};
+    border-radius: 4px;
+    
+    color: ${({theme}) => theme.colors.shapes.primary};
+    text-transform: uppercase;
+    font-weight: 400;
+
+
+
+    width: 100%;
+
+    &>svg {
+      margin-right: 8px;
+      path {
+        stroke: ${({theme}) => theme.colors.shapes.primary} !important;
+      }
+    }
+    
+  `;
+
   
