bac9bf44438584ce5530223252feb42a9a664ec0
Carlos Passos <carlospassos93@gmail.com>
2021-09-15 16:53:43 -0300

    style: change the back button main style

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -3,4 +3,13 @@ import styled from 'styled-components';
 
 export const Container = styled.button`
   display: flex;
+  align-items: center;
+  color: ${({theme}) => theme.colors.texts.light};
+  font-size: 14px;
+
+  cursor: pointer;
+
+  &>svg {
+    margin-right: 8px;
+  }
 `;
